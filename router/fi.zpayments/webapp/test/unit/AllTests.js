3e097dcb0adc175538d56174845ff11b09d88a64
JuanDarsaut <juanignacio.darsaut@cleverft.com>
2023-05-29 19:40:19 +0000

    commit inicial

---
+++ router/fi.zpayments/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"comtelefonicafiori/fi.zpayments/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
---
+++ router/fi.zpayments1/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.telefonica.fiori.fi.zpayments1.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
